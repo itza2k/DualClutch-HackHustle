100085ea48a2456557dab791feebae3499937b8f
itza2k <161753369+itza2k@users.noreply.github.com>
2024-11-09 03:22:04 +0530

    added settings

---
+++ Android-client/flowsync/app/src/main/java/com/hh/flowsync/ui/screens/SettingsScreen.kt
@@ -0,0 +1,26 @@
+package com.hh.flowsync.ui.screens
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun SettingsScreen() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text("Settings", style = MaterialTheme.typography.h4)
+        // Add additional settings components here
+    }
+}
